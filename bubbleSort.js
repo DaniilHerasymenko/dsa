40da38ad459b03555fda95d288acbd5adbe90495
DaniilHerasymenko <dangerasimenkostudy@gmail.com>
2024-01-15 19:30:41 +0200

    bubbleSort

---
+++ bubbleSort.js
@@ -0,0 +1,16 @@
+// сортування бульбашкою
+// Big O = O(n^2)
+const arr = [5,3,4,1,2];
+function bubbleSort(array){
+    for (let i = 0; i < array.length; i++){
+        for (let j = 0; j < array.length; j++){
+            if (array[j+1] < array[j]){
+                let temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+    console.log(array);
+}
+bubbleSort(arr);
